10fb5b3930ae102f84f5ab5168262693409edf7b
Aladin Virmaux <aladin.virmaux@gmail.com>
2016-07-13 10:22:08 +0200

    distance Manhattan

--- taquin/main.cpp
+++ taquin/main.cpp
@@ -4,16 +4,21 @@
 #include "search.hpp"
 #include "taquin.hpp"
 
+const int size_example = 3;
+
 int main() {
     srand(time(NULL));
-    Taquin::Board<3> taq;
-    std::vector<Taquin::Board<3>> shortest_path;
+    Taquin::Board<size_example> taq;
+    std::vector<Taquin::Board<size_example>> shortest_path;
 
-    taq.shuffle(100);
-    Taquin::SearchTree<3> searchTaq (taq);
-    shortest_path = searchTaq.a_star_search();
+    for (int i = 0; i < 20; i++) {
+        taq.shuffle(100);
+        Taquin::SearchTree<size_example> searchTaq (taq);
+        shortest_path = searchTaq.a_star_search();
+        std::cout << shortest_path.size() << std::endl;
+    }
 
-    Taquin::print_solution(shortest_path);
-    std::cout << shortest_path.size() << std::endl;
+    // Taquin::print_solution(shortest_path);
+    // std::cout << shortest_path.size() << std::endl;
     return 0;
 }
